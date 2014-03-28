0682f1c70fe483ec59acc4eda8840a5ee595b1d9
Luke Peters <movint@gmail.com>
2014-03-28 23:52:13 +0000

    Skeleton implementation of JMS classes

---
+++ movint-mq-client/src/main/java/movint/mq/client/jms/MovintMQConnectionFactory.java
@@ -0,0 +1,38 @@
+package movint.mq.client.jms;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+
+public class MovintMQConnectionFactory implements ConnectionFactory {
+	@Override
+	public Connection createConnection() throws JMSException {
+		return null;
+	}
+
+	@Override
+	public Connection createConnection(String userName, String password) throws JMSException {
+		return null;
+	}
+
+	@Override
+	public JMSContext createContext() {
+		return null;
+	}
+
+	@Override
+	public JMSContext createContext(String userName, String password) {
+		return null;
+	}
+
+	@Override
+	public JMSContext createContext(String userName, String password, int sessionMode) {
+		return null;
+	}
+
+	@Override
+	public JMSContext createContext(int sessionMode) {
+		return null;
+	}
+}
